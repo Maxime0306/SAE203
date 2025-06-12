d2b9401a49454b62af853c0574f89b345e881752
adrien <atritarelli@outlook.com>
2025-06-12 17:07:22 +0200

    rendu final

--- myweb_package/tmp/usr/local/sbin/start_myweb.sh
+++ myweb_package/tmp/usr/local/sbin/start_myweb.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Fichier : start_myweb.sh
+# Script : start_myweb.sh
 
-JAR_PATH="var\run\myweb\server.jar"
-CONF_PATH="etc\myweb\myweb.conf"
-LOG_PATH="myweb_access.log"
+JAR_PATH="/usr/local/lib/myweb/server.jar"
+CONF_PATH="/etc/myweb/myweb.conf"
+LOG_PATH="/usr/local/lib/myweb/myweb_access.log"
+PID_PATH="/var/run/myweb/myweb.pid"
 
 if [ ! -f "$JAR_PATH" ]; then
     echo "Erreur : fichier $JAR_PATH introuvable"
+    exit 1
 fi
 
 nohup java -cp "$JAR_PATH" HttpServer "$CONF_PATH" > "$LOG_PATH" 2>&1 &
+echo $! > "$PID_PATH"
 echo "Serveur lancé avec PID $!"
-echo $! > C:\Users\maxim\tmp\ari\var\run\myweb\myweb.pid
+
